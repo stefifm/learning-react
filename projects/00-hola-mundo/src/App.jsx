cd2d4992bf936f5384c3f92cc6bbbdc4bf9de021
stefifm <bruerastefania@gmail.com>
2023-01-15 21:20:47 -0300

    Adding array of users

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,10 +1,43 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: "stefifm",
+    name: "Stefania Bruera",
+    isFollowing: true
+  },
+  {
+    userName: "midudev",
+    name: "Miguel Angel Duram",
+    isFollowing: false
+  },
+  {
+    userName: "kikobeats",
+    name: "Kiko",
+    isFollowing: true
+  },
+  {
+    userName: "elonmusk",
+    name: "Elon Musk",
+    isFollowing: false
+  },
+]
 export function App() {
   return (
     <section className='App'>
-      <TwitterFollowCard userName='stefifm'>Stefania Bruera</TwitterFollowCard>
-      <TwitterFollowCard userName='midudev'>Miguel Angel Duran</TwitterFollowCard>
+      {
+        users.map(({userName, name, isFollowing}) => (
+          
+          <TwitterFollowCard
+          userName={userName}
+          initialIsFollowing={isFollowing}
+          key={userName}          
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
     </section>
   )
 }
